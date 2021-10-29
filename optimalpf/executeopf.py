509c15502ec91ecde744fc4af3dbb7fe4641b9e0
AnkurArohi <ankurarohi1@gmail.com>
2021-10-29 14:14:24 +0200

    Printing results and analysis

--- optimalpf/executeopf.py
+++ optimalpf/executeopf.py
@@ -2,22 +2,40 @@ import pandapower as pp
 from pandapowerTest.optimalpf.opf import Opf
 
 
-opf=Opf("Loss minimization")
-pp.runopp(opf.net, delta=1e-16)
+def analyzingresults():
+    print("OPF output Slack: \n", opf.net.res_ext_grid)
+    print("OPF output Gen: \n", opf.net.res_gen)
+    sum_p_gen = 0.0
+    sum_p_load = 0.0
+    for p in opf.net.res_gen.get("p_mw"):
+        sum_p_gen += p
+    for p in opf.net.res_ext_grid.get("p_mw"):
+        sum_p_gen += p
+    print("Total generation: ", sum_p_gen)
+    for p in opf.net.res_load.get("p_mw"):
+        sum_p_load += p
+    print("Total load: ", sum_p_load)
+    print("Total OPF cost :", opf.net.res_cost)
 
-print("OPF output Slack: \n",opf.net.res_ext_grid)
-print("OPF output Gen: \n",opf.net.res_gen)
+    print("#############-- Loading in trafo --########## \n")
+    print("After OPF Trafo Loading : ", opf.net.res_trafo.loading_percent)
+    print("#############-- Loading in lines --########## \n")
+    print("After OPF line loading: \n", opf.net.res_line.loading_percent)
 
-sum_p_gen=0.0
-sum_p_load=0.0
-for p in opf.net.res_gen.get("p_mw"):
-    sum_p_gen += p
-for p in opf.net.res_ext_grid.get("p_mw"):
-    sum_p_gen += p
+def permutethroughcostfunc():
+    global opf
+    print("--------------------- Loss Minimization------------------- \n")
+    opf = Opf("Loss minimization")
+    pp.runopp(opf.net, verbose=True, delta=1e-16)
+    analyzingresults()
+    print("--------------------- Slack Cheaper------------------------ \n")
+    opf = Opf("Slack cheaper")
+    pp.runopp(opf.net, verbose=True, delta=1e-16)
+    analyzingresults()
+    print("--------------------- Slack Minimization------------------- \n")
+    opf = Opf("avoid Slack")
+    pp.runopp(opf.net, verbose=True, delta=1e-16)
+    analyzingresults()
 
-print("Total generation: ",sum_p_gen)
 
-for p in opf.net.res_load.get("p_mw"):
-    sum_p_load += p
-
-print("Total load: ",sum_p_load)
+permutethroughcostfunc()
--- optimalpf/opf.py
+++ optimalpf/opf.py
@@ -1,18 +1,35 @@
 from pandapowerTest.topology.topologicalfeature import Topology
 import pandapower as pp
 
+
 class Opf(Topology):
     def __init__(self, name):
         super().__init__(name)
-        pp.runpp(self.net,init="auto")
-        print("Before OPF Slack : ",self.net.res_ext_grid)
-        print("Before OPF Gen : ",self.net.res_gen)
+        pp.runpp(self.net, init="auto")
+        print("Before OPF Slack : ", self.net.res_ext_grid)
+        print("Before OPF Gen : ", self.net.res_gen)
         print("Before OPF Load : ", self.net.res_load)
+        print("Before OPF Trafo Loading : \n", self.net.res_trafo.loading_percent)
+        print("Before OPF line loading: \n",self.net.res_line.loading_percent)
+        self.costfunc(name)
+        self.modifyconstraints(80, 80)
 
-        self.costfunc()
+    def costfunc(self, name):
+        if (name == "Loss minimization"):
+            costeg = pp.create_poly_cost(self.net, 0, 'ext_grid', cp1_eur_per_mw=10)
+            costgen1 = pp.create_poly_cost(self.net, 0, 'gen', cp1_eur_per_mw=10)
+            costgen2 = pp.create_poly_cost(self.net, 1, 'gen', cp1_eur_per_mw=10)
+        elif (name == "Slack cheaper"):
+            costeg = pp.create_poly_cost(self.net, 0, 'ext_grid', cp1_eur_per_mw=1)
+            costgen1 = pp.create_poly_cost(self.net, 0, 'gen', cp1_eur_per_mw=10)
+            costgen2 = pp.create_poly_cost(self.net, 1, 'gen', cp1_eur_per_mw=10)
+        elif (name == "avoid Slack"):
+            costeg = pp.create_poly_cost(self.net, 0, 'ext_grid', cp1_eur_per_mw=10, cp2_eur_per_mw2=1)
+            costgen1 = pp.create_poly_cost(self.net, 0, 'gen', cp1_eur_per_mw=1)
+            costgen2 = pp.create_poly_cost(self.net, 1, 'gen', cp1_eur_per_mw=1)
+        else:
+            print("Select a optimizing cost poly type")
 
-    def costfunc(self):
-        costeg = pp.create_poly_cost(self.net, 0, 'ext_grid', cp1_eur_per_mw=10)
-        costgen1 = pp.create_poly_cost(self.net, 0, 'gen', cp1_eur_per_mw=10)
-        costgen2 = pp.create_poly_cost(self.net, 1, 'gen', cp1_eur_per_mw=10)
-
+    def modifyconstraints(self, trafo, line):
+        self.net.trafo["max_loading_percent"] = trafo
+        self.net.line["max_loading_percent"] = line
